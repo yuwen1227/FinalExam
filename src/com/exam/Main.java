526060848d7e75b33e8cd876ef83be80caea8667
yuwen1227 <coco4522@gmail.com>
2018-06-26 15:40:51 +0800

    完成

--- src/com/exam/Main.java
+++ src/com/exam/Main.java
@@ -4,11 +4,17 @@ import java.io.BufferedReader;
 import java.io.FileNotFoundException;
 import java.io.FileReader;
 import java.io.IOException;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Scanner;
 
 public class Main {
-
+	Scanner scanner = new Scanner(System.in);
+	
 	public static void main(String[] args) {
+		
 		Menu m = new Menu();
+		
 		try {
 			FileReader f = new FileReader("menu.txt");
 			BufferedReader in = new BufferedReader(f);
@@ -16,11 +22,29 @@ public class Main {
 			String[] tokens = line.split(",");
 			int count = Integer.parseInt(tokens[0]);
 			for(int i=0;i<count;i++){
+				int id =Integer.parseInt(tokens[0]);
 				String name = tokens[1];
 				int price = Integer.parseInt(tokens[2]);
 				int kcal = Integer.parseInt(tokens[3]);
-				m.f.add(new Food(i+1,name ,price ,kcal));
+				line = in.readLine();
+				m.foods.add(new Food(i+1,name ,price ,kcal, kcal));
+			
+				}
+			
+			
+		/*	line = in.readLine();
+			String[] tokens2 = line.split(",");
+			List<String> list2 = new ArrayList<>();
+			for(int i=0;i<tokens2.length;i++){
+				list2.add(tokens[i]);
 			}
+			line = in.readLine();
+			String[] tokens3 = line.split(",");
+			List<String> list3 = new ArrayList<>();
+			for(int i=0;i<tokens3.length;i++){
+				list3.add(tokens[i]);
+			}
+		*/
 			m.on();
 			
 			
--- src/com/exam/Menu.java
+++ src/com/exam/Menu.java
@@ -2,17 +2,54 @@ package com.exam;
 
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Scanner;
 
 public class Menu {
-	List<Food> f = new ArrayList<>();
-	
-		public void on(){
-			for(int a=0;a<f.size();a++){
-			Food food = f.get(a);
-			System.out.println(food.getId()+"."+"\t"+food.getName()+food.getPrice()+food.getKcal());
+	List<Food> foods = new ArrayList<>();
+
+	public void on() {
+		for (int a = 0; a < foods.size(); a++) {
+			Food food = foods.get(a);
+			System.out
+					.println(food.getId() + "." + food.getName() + "  " + " " + food.getPrice() + " " + food.getKcal());
 			System.out.println("請輸入餐點:");
 			System.out.println("請輸入數量:");
-			
+		}
+
+		Scanner scanner = new Scanner(System.in);
+		String food = scanner.nextLine();
+		int data = -1;
+		while (data != 0) {
+			data = scanner.nextInt();
+			System.out.println("請輸入餐點:");
+			String name = scanner.nextLine();
+			switch (data) {
+			case 1:
+				if(name != null){
+				System.out.println("請輸入數量:");
+					}else{
+						
+					}
+				break;
+			case 2:
+				System.out.println("請輸入數量:");
+				
+			case 3:
+				System.out.println("請輸入數量:");
+				
+			case 4:
+				System.out.println("請輸入數量:");
+				
+			case 5:
+				System.out.println("請輸入數量:");
+				
+			case 6:
+				System.out.println("請輸入數量:");
+				
+			case 7:
+				System.out.println("請輸入數量:");
+
 			}
 		}
+	}
 }
